775af1a4879f8a7feb346406534cf405384095cd
Will CD <chavezdiaz4@gmail.com>
2021-11-06 00:16:33 -0500

    Conociendo react

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//Aca contenemos las librerias de react
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
+//Aca contenemos la vista
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hola mundo!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Aca contenemos los estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 50,
   },
 });
