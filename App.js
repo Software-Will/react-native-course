67d091949f85c60d189a825c708ccbc9bfd6e3e5
Will CD <chavezdiaz4@gmail.com>
2021-11-06 02:22:13 -0500

    Image Picker - Expo.io - validamos el acceso y cambiamos imgs de forma volatil

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //Aca contenemos las librerias de react
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react"; //libreria de react
 import {
   StyleSheet,
   Text,
@@ -10,20 +10,44 @@ import {
   TouchableOpacity,
 } from "react-native"; //Cada que implemento una nueva etiqueta debo de importarlo
 import pythonImage from "./assets/py.png"; //en assets se guardan las img locales
+import * as ImagePicker from "expo-image-picker";
 
 //Aca contenemos la vista
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null); //Estado para cambiar la img de la vista
+
+  //Funcion para pedir un permiso, asincrono
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync(); //Pide permiso a la galeria
+
+    if (permissionResult.granted === false) {
+      alert("Se requiere el acceso a su camara");
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hola mundo!!!</Text>
       <Image
-        source={{ uri: "https://picsum.photos/200/200" }} //imagenes que no son de internet lo defines import nameImage y la direccion es source={imgName}
+        source={{
+          uri:
+            selectedImage !== null
+              ? selectedImage.localUri
+              : "https://picsum.photos/200/200",
+        }} //imagenes que no son de internet lo defines import nameImage y la direccion es source={imgName}
         style={styles.image}
       />
-      <TouchableOpacity
-        onPress={() => Alert.alert("Presionaste el boton")}
-        style={styles.button}
-      >
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Ingresar</Text>
       </TouchableOpacity>
     </View>
@@ -46,6 +70,7 @@ const styles = StyleSheet.create({
     height: 200,
     width: 200,
     borderRadius: 100, //la mitad de la imagen pq no funcion %
+    resizeMode: "contain",
   },
   button: {
     backgroundColor: "#2f4f4f",
