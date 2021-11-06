6759a4da7c74cccbd808f04aef7081cd5a4759b4
Will CD <chavezdiaz4@gmail.com>
2021-11-06 00:46:14 -0500

    Implementacion de imgs y estilos

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 //Aca contenemos las librerias de react
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native"; //Cada que implemento una nueva etiqueta debo de importarlo
+import pythonImage from "./assets/py.png"; //en assets se guardan las img locales
 
 //Aca contenemos la vista
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hola mundo!!!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: "https://picsum.photos/200/200" }} //imagenes que no son de internet lo defines import nameImage y la direccion es source={imgName}
+        style={styles.image}
+      />
+      <Button
+        onPress=""
+        title="Ingrese"
+        color="#222"
+        accessibilityLabel="Esto es un boton"
+      />
     </View>
   );
 }
@@ -17,11 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#333",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     fontSize: 50,
+    color: "#fff",
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100, //la mitad de la imagen pq no funcion %
   },
 });
